a100a641a36beb35fe6f698a7107ec4755904e77
Zoltan Gero <humorketing@gmail.com>
2018-10-11 11:56:46 +0100

    Excercise 5 is abotu vars interpolation and how to use quotes and double quotes

---
+++ ex5.rb
@@ -0,0 +1,17 @@
+my_name = 'Zoltan the superhero'
+my_age = 33 # in 2018
+my_height = 175 # cms
+my_weight = 90 # kgs
+my_eyes = 'Brown'
+my_teeth = 'White'
+my_hair = 'Bold'
+
+puts "Hey my name is #{my_name}."
+puts "He's #{my_height} centi tall."
+puts "He's #{my_weight} kgs heavy."
+puts "Actually I'm too heavy."
+puts "He's got #{my_eyes} eyes and #{my_hair} hair."
+puts "His teeth are usually #{my_teeth} depending on the coffee."
+
+# this line is tricky, try to get it exactly right
+puts "If I add #{my_age}, #{my_height}, and #{my_weight} I get #{my_age + my_height + my_weight}."
